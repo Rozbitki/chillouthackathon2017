ed9a8ced81245c33306a02f9e87f4a738ae5d053
Mestru <Mestruex@gmail.com>
2017-11-24 23:20:22 +0100

    added more api methods and soapui testing file

--- src/main/java/app/controllers/ApiController.java
+++ src/main/java/app/controllers/ApiController.java
@@ -1,5 +1,6 @@
 package app.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +9,35 @@ public class ApiController extends BaseController {
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public String getUser(@PathVariable Long id){
+    public ResponseEntity getUser(@PathVariable Long id){
         String returnString = "{\"id\": " + id + ", \"name\": \"Janusz\"}";
-        return returnString;
+        return ResponseEntity.ok(returnString);
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity deleteUser(@PathVariable Long id){
+        String returnString = "{\"id\": " + id + ", \"name\": \"Janusz\"}";
+        return ResponseEntity.ok(returnString);
+    }
+
+    @RequestMapping(value = "/advertisement/{id}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getAdvertisement(@PathVariable Long id){
+        String returnString = "{\"id\": " + id + ", \"name\": \"Janusz\"}";
+        return ResponseEntity.ok(returnString);
+    }
+
+    @RequestMapping(value = "/advertisements", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getAdvertisement(@RequestBody String request){
+        return ResponseEntity.ok(request);
+    }
+
+    @RequestMapping(value = "/advertisement", method = RequestMethod.PUT, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity addAdvertisement(@RequestBody String request){
+        return ResponseEntity.ok(request);
     }
 
 }

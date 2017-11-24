061ef6c621c97350ca0e95cb19603230fe9c2fea
Tomekku <thetomekku@gmail.com>
2017-11-24 19:53:28 +0100

    test

---
+++ src/main/java/Application.java
@@ -0,0 +1,19 @@
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = (ApplicationContext) SpringApplication.run(Application.class,args);
+    }
+
+}

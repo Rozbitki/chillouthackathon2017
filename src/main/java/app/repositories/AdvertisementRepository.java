7784bfc802b76eb29429b276308e6abf2e304583
Tomekku <thetomekku@gmail.com>
2017-11-25 08:45:48 +0100

    add some methods

--- src/main/java/app/repositories/AdvertisementRepository.java
+++ src/main/java/app/repositories/AdvertisementRepository.java
@@ -1,12 +1,16 @@
 package app.repositories;
 
+import app.dto.AdvertiseShort;
 import app.entities.Advertisement;
 import app.entities.AgeGroup;
 import app.enums.ExpGroup;
 import app.enums.SportDiscipline;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,4 +20,5 @@ public interface AdvertisementRepository extends CrudRepository<Advertisement, L
     public List<Advertisement> findTop10ByExpGroupAndDeletedFalseOrderByEndTimeAsc(ExpGroup expGroup);
     public List<Advertisement> findTop10ByAgeGroupAndDeletedFalseOrderByEndTimeAsc(AgeGroup ageGroup);
     public List<Advertisement> findTop10BySportDisciplineAndDeletedFalseOrderByEndTimeAsc(SportDiscipline sportDiscipline);
+    public List<Advertisement> findAll();
 }
